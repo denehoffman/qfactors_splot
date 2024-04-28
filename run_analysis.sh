09fb701fa1ef086f2a7771cb5c25bc0d5ce0dc15
Zach Baldwin <zbaldwin@ernest.phys.cmu.edu>
2024-04-27 21:49:40 -0400

    Update analysis to handle SLURM usage if necessary. Also fixed slight issue with results exec to handle NaN.

--- run_analysis.sh
+++ run_analysis.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
-# Passed argument from the main submit script
+echo -------------------SLURM JOB INFO-----------------------------
+echo 'Submitted from' $SLURM_SUBMIT_HOST
+echo 'on' $SLURM_JOB_PARTITION 'queue'
+echo 'Job identifier is' $SLURM_JOB_ID
+echo 'Job name is' $SLURM_JOB_NAME
+echo 'The username is' $SLURM_JOB_USER
+echo -n 'Job is running on node '; echo $SLURM_JOB_NODELIST
+echo --------------------------------------------------------------
+
 ITERATION=$1
 
-SCRIPT_DIR="/home/zbaldwin/qfactors_splot"
+OUTPUT_DIR="${SCRIPT_DIR}/slurm"                                                                                                                                                                      
+mkdir -p "$OUTPUT_DIR"
 
 # Run the Python script with the current iteration
-python3.11 "$SCRIPT_DIR/analysis" --num-sig 100 --num-bkg 100 --num-iter "$ITERATION"
+python3.11 "$SCRIPT_DIR/analysis" --num-sig 1000 --num-bkg 1000 --num-iter "$ITERATION" --output "$OUTPUT_DIR" 
 
--- submit_jobs.sh
+++ submit_jobs.sh
@@ -1,15 +1,58 @@
 #!/bin/bash
 
-# Number of iterations to run
-NUM_ITERATIONS=10
+# Current username
+USERNAME=$(whoami)
 
 # Directory where sQ-factors analysis script is located
 SCRIPT_DIR="/home/zbaldwin/qfactors_splot"
 
-QUEUE=red
+DEFAULT_QUEUE="red"
+DEFAULT_NUM_ITERATIONS=3     
 
-SLEEP=3
+RUN_PARTITION_UPDATE=false
 
+usage() {
+    echo "Usage: $0 [options]"
+    echo "Options:"
+    echo "  --queue <queue>             Specify the Slurm queue (default: $DEFAULT_QUEUE)"
+    echo "  --num-iter <count>          Specify the number of iterations (default: $DEFAULT_NUM_ITERATIONS)"
+    echo "  --update-partition          Update the partition for pending jobs"
+    echo "  --help                      Display this help message"
+    exit 1
+}
+
+if [[ "$1" == "--help" ]]; then
+    usage 
+fi
+
+# Parse command-line arguments
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --queue)
+            QUEUE="$2"
+            shift
+            ;;
+            --num-iter)
+            NUM_ITERATIONS="$2"
+            shift
+            ;; 
+            --update-partition)
+            RUN_PARTITION_UPDATE=true
+            ;;
+        *)  # Unknown option
+            echo "Unknown option: $1"
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+# Use default queue and iterations if no input provided
+QUEUE=${QUEUE:-$DEFAULT_QUEUE}
+NUM_ITERATIONS=${NUM_ITERATIONS:-$DEFAULT_NUM_ITERATIONS}
+
+# Usage for CMU MEG cluster
 if [ "$QUEUE" == "green" ]; then
   CPUMEM=1590
   THREADS=40
@@ -24,19 +67,33 @@ else
 fi
 
 let MEM=$THREADS*CPUMEM
+SLEEP=1
+
+# Update partition if specified
+if $RUN_PARTITION_UPDATE; then
+    read -p 'Which partition do you want to switch to? ': partvar
+    for i in $(squeue -u $USERNAME -h -t PD -o %i); do
+        scontrol update jobid=$i partition=$partvar
+    done
+    exit 
+fi
+
 
 # Loop through the number of iterations
-for (( i=0; i<$NUM_ITERATIONS; i++ ))
+for (( i=1; i<($NUM_ITERATIONS+1); i++ ))
 do
+    LOG_DIR="${SCRIPT_DIR}/log/${i}"    
+    mkdir -p "$LOG_DIR"      
+
     # Submit the job to Slurm 
-    sbatch --export=ALL,ITERATION=$i,SCRIPT_DIR=$SCRIPT_DIR \
+    sbatch --export=ALL,ITERATION=$i,SCRIPT_DIR=$SCRIPT_DIR,LOG_DIR=$LOG_DIR \
            --job-name="sQ_factor_iter_$i" \
            --ntasks=${THREADS} \
            --partition=${QUEUE} \
            --mem=${MEM} \
            --time=02:00:00 \
-           --output="${SCRIPT_DIR}/slurm_results/qfactor_splot_iter_${i}_%j.out" \
-           --error="${SCRIPT_DIR}/slurm_results/qfactor_splot_iter_${i}_%j.err" \
+           --output="${LOG_DIR}/qfactor_splot_iter_${i}.out" \
+           --error="${LOG_DIR}/qfactor_splot_iter_${i}.err" \
            "${SCRIPT_DIR}/run_analysis.sh" $i
-    sleep $SLEEP
+    #sleep $SLEEP
 done
